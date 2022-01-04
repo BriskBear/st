f8dde75760a2a7878d5836fb4da23689af9da13d
deftclaw <eeccher@cppwind.com>
2022-01-04 10:42:53 -0700

    update font-size

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Terminus:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Terminus:pixelsize=24:antialias=true:autohint=true";
 static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 2;
 
