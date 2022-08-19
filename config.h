91c6944f28ac5bfd22a895bc66caada012c75c8c
Elijah D Eccher <eeccher@cppwind.com>
2022-08-19 08:05:43 -0600

    Add font, adjust font-size, colorscheme

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Terminus:pixelsize=18:antialias=true:autohint=true";
+static char *font = "Terminus:pixelsize=15:antialias=true:autohint=true";
 static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 2;
 
